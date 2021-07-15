f7972e16a813f39bc24dcdfe34dbf6740eec51a7
Mahmud-Sufian <mahmud5281585@gmail.com>
2021-07-15 13:19:21 +0600

    nav and slider

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../Nav/Nav'
+import Slider from '../Slider/Slider'
+
+function Home() {
+    return (
+        <div>
+           <Nav></Nav> 
+           <Slider></Slider>
+        </div>
+    )
+}
+
+export default Home
